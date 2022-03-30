5ceacce4001afbe81bcce96d0434eae1afeba8a1
crystal1229d <crystal1229d@naver.com>
2022-03-30 23:10:36 +0900

    2022-03-30

---
+++ Algorithm/01-Array_And_Object/00-Indtroduction.js
@@ -0,0 +1,28 @@
+/* 
+
+    [Udemy] Javascript Algorithm
+
+    =============================
+
+    Section 2. Performance of Array & Object
+
+    2-1 Introduction 
+
+    =============================
+
+    < BUILT-IN DATA STRUCTURES through the lens of Big O >
+
+    Now that we've covered Big O,
+    Let's spend a couple minutes analyzing the things we do all the time in JS:
+    working with Arrays, Objects, and built-in methods.
+
+    
+
+    < OBJECTIVES >
+    * Understand how objects and arrays work, through the lens of Big O
+    * Explain why adding elements to the beginning of an array is costly (좋지 않은 이유 + 더 좋은 대안 + 배열 처리 시간 비교)
+    * Compare and contrast the runtime for arrays and objects, as well as built-in methods
+        (example of built-in methods) : sort, forEach, Object.key, Object.hasOwnProperty, ...
+
+
+*/
---
+++ Algorithm/01-Array_And_Object/01-ObjectsBigO.js
@@ -0,0 +1,25 @@
+/* 
+
+    [Udemy] Javascript Algorithm
+
+    =============================
+
+    Section 2. Performance of Array & Object
+
+    2-1 Object's Big O
+
+    =============================
+
+
+    < OBJECTS >
+    Unordered, key-value pairs!
+
+    let instructor = {
+        firstName: "Keyll",
+        isInstructor: true,
+        favoriteNumbers: [ 1,2,3,4 ]
+    }
+
+
+
+*/
